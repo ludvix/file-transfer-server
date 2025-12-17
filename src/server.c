f9bdf5c1ec8ec17a51a4092b7bf47f5d38e06782
Ludvig <kalbergludvig@outlook.com>
2025-12-17 15:26:42 +0100

    Add server_create function with tcp socket and address creation

--- src/server.c
+++ src/server.c
@@ -1,6 +1,40 @@
 #include "server.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-void server(void){
-    printf("Hello\n");
+int server_create(struct server *srv){
+    srv->listen_fd = socket(AF_INET, SOCK_STREAM, 0); // TCP socket
+    if (srv->listen_fd < 0) {
+        perror("Socket creation failed -_-");
+        return -1;
+    }
+    srv->client_fd = -1; // No client connected yet
+
+    memset(&srv->serv_address, 0, sizeof(srv->serv_address)); // Clear garbage
+    srv->serv_address.sin_family = AF_INET;
+    srv->serv_address.sin_addr.s_addr = INADDR_ANY;
+    srv->serv_address.sin_port = 0; // Port will be set in server_start
+
+    return 0;
+}
+
+int server_start(struct server *srv, int port){
+
+}
+
+int server_listen(struct server *srv){
+
+}
+
+int server_accept(struct server *srv){
+
+}
+
+int handle_connection(struct server *srv){
+
+}
+
+void server_close(struct server *srv){
+
 }
