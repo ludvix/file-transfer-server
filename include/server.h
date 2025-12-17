8c08aa6365fcbe12265840745f9b2f6aea08607c
Ludvig <kalbergludvig@outlook.com>
2025-12-17 14:55:08 +0100

    Add server struct, with neccesary info

--- include/server.h
+++ include/server.h
@@ -1,6 +1,16 @@
 #ifndef SERVER_H
 #define SERVER_H
 
-void server(void);
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+struct server{
+    int listen_fd;
+    int client_fd;
+    struct sockaddr_in serv_address;
+    struct sockaddr_in client_address;
+    socklen_t client_address_length;
+};
 
 #endif
+
