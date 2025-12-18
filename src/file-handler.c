1ceca4831f0a7120b1bb10cca0147ec14b6df230
Ludvig <kalbergludvig@outlook.com>
2025-12-18 20:58:45 +0100

    Add function send file, logic open file binary mode

--- src/file-handler.c
+++ src/file-handler.c
@@ -1,6 +1,12 @@
 #include "file-handler.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-void file_handler(void){
-    printf("Hello\n");
+ // Open file in binary mode
+    int send_file(struct server *srv, char *buffer){
+    FILE *file_from_server = fopen(buffer, "rb");
+    if (file_from_server == NULL) {
+        perror("File open failed -_-");
+        return -1;
+    }
 }
